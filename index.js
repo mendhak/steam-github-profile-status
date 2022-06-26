744990c2234e6f1bc40cbce0f5586006ce580e34
mendhak <mendhak@users.noreply.github.com>
2022-06-26 14:07:05 +0100

    Update Github Status

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-
 var SteamCommunity = require('steamcommunity');
-
-
+var githubProfileStatus = require('github-profile-status');
 
 let community = new SteamCommunity();
-community.getSteamUser(new SteamCommunity.SteamID('76561197984170060'), (err, user) => {
+community.getSteamUser(new SteamCommunity.SteamID('76561197962020667'), (err, user) => {
+    console.debug(user);
 
     if(user.onlineState === 'in-game'){
-        console.debug(user.stateMessage);
+
         var currentGame = user.stateMessage.split(">")[1];
         if(currentGame.length > 0){
             console.log(`User is in-game: ${currentGame}`);
+            const profileStatus = new githubProfileStatus.GitHubProfileStatus({
+                token: process.env.GITHUB_ACCESS_TOKEN,
+            });
+
+            var expiresAt = new Date();
+            expiresAt.setHours(expiresAt.getHours() + 1);
+
+            profileStatus.set({
+                emoji: ':video_game:',
+                message: currentGame,
+                limitedAvailability: true,
+                expiresAt: expiresAt,
+            }).then((stat) => {
+                console.log(stat);
+            });
+
         }
         
     }
